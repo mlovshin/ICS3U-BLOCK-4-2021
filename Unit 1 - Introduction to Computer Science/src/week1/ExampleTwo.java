03e2b6259329abbcb998ea13929bb5af9d2a8b2a
Muriel Lovshin <mlovshin@bayviewglen.ca>
2021-09-09 15:18:56 -0400

    Initial Commit

---
+++ Unit 1 - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+
+}
