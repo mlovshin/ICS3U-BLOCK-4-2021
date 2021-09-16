6289b016df5882949ec84be76ef7680fc70471f1
Muriel Lovshin <mlovshin@bayviewglen.ca>
2021-09-16 14:57:46 -0400

    Unit 2 - Using Objects

---
+++ Unit 1 - Introduction to Computer Science/Unit 2 - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,25 @@
+package week2;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student fred = new Student("Fred", "654321", 10 );  // fred is a reference to a Student in brackets
+        Student max = new Student("Max", "12345", 10); // max is a refernce to a Student in brackets
+        Student muriel = fred; // assigning muriel to fred with the same Student in brackets 
+        muriel.increaseGrade();
+
+        fred = null;
+       // java throws a NullPointerException, fred doesn't equal Student anymore but muriel still has the value because she has an arrow to the bubble
+        //non-static methods belongs to the OBJECT(Student) not the CLASS(public static void main)
+        // attributes define the state
+
+        max.addTest(87);
+        max.addTest(94);
+        max.addTest(98);
+        max.addTest(67);
+        max.displayAverage();
+
+    }
+    
+
+    
+}
