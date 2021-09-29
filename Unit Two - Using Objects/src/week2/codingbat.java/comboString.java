fec32c3b1eb73b030175b22e7c207b175a0b5b9a
Muriel Lovshim <mlovshim@bayviewglen.ca>
2021-09-28 20:42:52 -0400

    codingbat

---
+++ Unit Two - Using Objects/src/week2/codingbat.java/comboString.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class comboString {
+    public static void main(String[] args){ 
+        public String comboString(String a, String b) {
+            int numA = a.length();
+            int numB = b.length();
+            
+            if (numA < numB)
+              return a + b + a;
+              else
+                return b + a + b;
+              
+              
+              
+             
+            }
+          
+          
+        
+    }
+
+   
