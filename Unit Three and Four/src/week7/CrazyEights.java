70bd0f064d53615140e1272d254485a54684ffe1
Muriel Lovshim <mlovshim@bayviewglen.ca>
2021-10-24 19:47:52 -0400

    week7

--- Unit Three and Four/src/week7/CrazyEights.java
+++ Unit Three and Four/src/week7/CrazyEights.java
@@ -2,6 +2,8 @@ package week7;
 
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class CrazyEights {
     private static final double NUM_SUITS = 4;
     private static final String HEARTS = "H";
@@ -13,7 +15,12 @@ public class CrazyEights {
     private static final String JACK = "J";
     private static final String QUEEN = "Q";
     private static final String KING = "K";
-    private static final int playerHand = 0;
+    private static String playerHand = "";
+    private static String comp1 = "";
+    private static String comp2 = "";
+    private static String player1 = playerHand;
+    private static String compPlayer1 = comp1;
+    private static String compPlayer2 = comp2;
 
 
     
@@ -23,20 +30,65 @@ public class CrazyEights {
 		getSuit();
         getFace();
         getCard();
-        System.out.println(getSuit() + "" + getFace() + "" + getCard() + "");
-        System.out.println("Player = " + playerHand);
-
+        
         playerHand = discard(in, playerHand);
         
     }
 
-private static String getCard() {
-    String card = getFace() + getSuit();
-    card = getFace() + getSuit();
-        return card;
+    private static String order(String player1, String compPlayer1, String compPlayer2) {
+        //how do I create an order of people, how do I connect this method to the "nextPlayer" method
     }
 
-    
+    private static String distributeCard(int playerHand, int comp1, int comp2) {
+    playerHand += (getCard() + getCard() + getCard() + getCard() + getCard());
+    comp1 += (getCard() + getCard() + getCard() + getCard() + getCard());
+    comp2 += (getCard() + getCard() + getCard() + getCard() + getCard());
+
+    System.out.println("Player hand is: " + playerHand);
+    }
+
+    private static String playCrazyEight(String playerHand, String comp1, String comp2) {
+        String topCard = "";
+        Object rank;
+        Object suits;
+        String nextPlayer = "";
+        if (playerHand.equals(8)){
+            return topCard += playerHand;
+            System.out.println("What suit does the next person have to play?: ");
+        if (nextPlayer) //code how to ask the next player to play that suit
+        else if (comp1.equals(8))
+            return topCard+= comp1;
+            System.out.println("What suit does the next person have to play?: ");
+        else if (comp2.equals(8))
+            return topCard+= comp2;
+            System.out.println("What suit does the next person have to play?: ");
+
+        } else if (suits.equals(playerHand)){
+            return topCard+= playerHand;
+        else if (suits.equals(comp1))
+            return topCard+= comp1;
+        else if (suits.equals(comp2))
+            return topCard += comp2;
+        else // what happens if not
+        
+        }else if(rank.equals(playerHand)) {
+                return topCard += playerHand;
+        else if(rank.equals(comp1))
+                return topCard += comp1;
+        else if(rank.equals(comp2))
+                return topCard += comp2;
+        else // what happens if not
+        
+    }
+
+
+    private static int discard(Scanner in, int playerhand2) {
+        return 0;
+    }
+
+    private static void getCard() {
+    }
+
     private static String getFace() {
         int face = (int)(Math.random() * NUM_CARDS_PER_SUIT) + 1;
 
@@ -65,43 +117,7 @@ private static String getCard() {
                     
         }
     
-    private static String discard(Scanner in, String playerHand) {
-        
-    
-            int numCardsToReplace = getNumberToDiscard(in);
-            String temp = playerHand;
-            if (numCardsToReplace == 0)
-               return playerHand;
-            else if (numCardsToReplace == 3) {
-               playerHand = getCard(playerHand + temp) + " ";
-               playerHand += getCard(playerHand + temp) + " ";
-               playerHand += getCard(playerHand + temp) + " ";
-            } else {
-               String cardToReplace = getCardsToDiscard(in, numCardsToReplace, playerHand);
-               int space = cardToReplace.indexOf(" ");
-               String card1 = "";
-               if (space == -1)
-                  card1 = cardToReplace;
-               else
-                  card1 = cardToReplace.substring(0, space);
-      
-               String card = getCard(playerHand + temp);
-               playerHand = playerHand.replace(card1, card);
-               if (numCardsToReplace == 2) {
-                  String card2 = cardToReplace.substring(space + 1);
-                  card = getCard(playerHand + cardToReplace + temp);
-                  playerHand = playerHand.replace(card2, card);
-               }
-            }
-            return playerHand;
-         }
-      
-      
-         private static String getCardsToDiscard(Scanner in, int numCardsToReplace, String playerHand2) {
-        return null;
-    }
-
-        private static String getNumberToDiscard(Scanner in, int numCardsToReplace, String playerHand) {
+    private static String getCardsToDiscard(Scanner in, int numCardsToReplace, String playerHand2) {
             final String VALID_CARDS = "AS2S3S4S5S6S7S8S9S10SJSQSKSAC2C3C4C5C6C7C8C9C10CJCQCKCAD2D3D4D5D6D7D8D9D10DJDQDKDAH2H3H4H5H6H7H8H9H10HJHQHKH";
             boolean validInput = false;
             String cards = "";
@@ -109,9 +125,8 @@ private static String getCard() {
                System.out.println("Please enter the cards to replace: (ex. 7H KD): ");
                cards = in.nextLine().toUpperCase();
               
-                String space = "";
-                String card1 = cards.substring(0, space);
-                  String card2 = cards.substring(space + 1);
+                String card1 = cards.substring(0);
+                  String card2 = cards.substring(0 + 1);
                   if (VALID_CARDS.indexOf(card1) < 0)
                      System.out.println("Not a valid card: " + card1);
                   else if (VALID_CARDS.indexOf(card2) < 0)
@@ -125,8 +140,16 @@ private static String getCard() {
                   else
                      validInput = true;
                }
-
+            return cards;
     }
+
+        private static String getCard(String string) {
+        String card = getFace() + getSuit();
+        card = getFace() + getSuit();
+            return card;
+    
+    }
+
 }
     
     
